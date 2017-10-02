9665bdc052f926b8d9559a73040277f2c443a935
MikkoRantakallio <mikko.rantakallio@gmail.com>
2017-10-02 18:30:56 +0300

    Working :-) Not the most sophisticated user interface...

--- weather-api/Program.cs
+++ weather-api/Program.cs
@@ -1,27 +1,14 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Ekoodi.Weather.API
 {
     class Program
     {
-//        static HttpClient client = new HttpClient();
         static Weather weather = new Weather();
-
-        static CurrentWeather localWeather;
-        static Forecast5days forecast;
 
         static void Main()
         {
             bool exit = false;
-/*            string url = "http://api.openweathermap.org/data/2.5/";
-            client.BaseAddress = new Uri(url);
-            client.DefaultRequestHeaders.Accept.Clear();
-            */
             do
             {
                 Console.Write("Give a city: (X=Exit): ");
@@ -33,64 +20,53 @@ namespace Ekoodi.Weather.API
                 if (!exit && cityInput.Length>0)
                 {
                     // Get current weather
-                    GetWeatherAsync(client.BaseAddress + "weather?q=" + cityInput).Wait();
+                    CurrentWeather localWeather = weather.GetWeather(cityInput);
 
-                    Console.WriteLine("");
-                    Console.WriteLine("Local weather in {0}:", localWeather.LocationName);
-                    Console.WriteLine("Temperature:\t{0} celcius", Math.Round(localWeather.MainData.Temperature-272.15));
-                    Console.WriteLine("Wind speed:\t{0} m/s", Math.Round(localWeather.Wind.Speed));
-
-                    foreach (WeatherDescription descr in localWeather.Descriptions)
+                    if (localWeather.Cod == "200")
                     {
-                        Console.WriteLine("ID:\t\t{0}", descr.Id);
-                        Console.WriteLine("Description:\t{0}", descr.GroupWeatherCondition);
+                        Console.WriteLine("");
+                        Console.WriteLine("Local weather in {0}:", localWeather.LocationName);
+                        Console.WriteLine("Temperature:\t{0} celcius", Math.Round(localWeather.MainData.Temperature-272.15));
+                        Console.WriteLine("Wind speed:\t{0} m/s", Math.Round(localWeather.Wind.Speed));
+
+                        foreach (WeatherDescription descr in localWeather.Descriptions)
+                        {
+                            Console.WriteLine("ID:\t\t{0}", descr.Id);
+                            Console.WriteLine("Description:\t{0}", descr.GroupWeatherCondition);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error in getting weather data!");
                     }
 
                     // Get forecast for five days
-                    GetForecastAsync(client.BaseAddress + "forecast?q=" + cityInput).Wait();
+                    Forecast5days forecast = weather.GetForecast(cityInput);
 
-                    Console.WriteLine("");
-                    Console.WriteLine("5 days / 3h forecast for {0}:", forecast.City.CityName);
-
-                    foreach( Forecast fc in forecast.ForecastList)
+                    if (forecast.Cod == "200")
                     {
-                        Console.WriteLine(fc.TimeTxt);
-                        Console.WriteLine("Temperature:\t{0} celcius", Math.Round( fc.MainData.Temperature - 272.15));
-                        Console.WriteLine("Wind speed:\t{0} m/s from direction: {1}", Math.Round(fc.Wind.Speed), Math.Round(fc.Wind.Degree));
-
-                        foreach (WeatherDescription wd in fc.Descriptions)
-                        {
-                            Console.WriteLine("Description:\t{0}", wd.GroupWeatherCondition);
-                        }
                         Console.WriteLine("");
+                        Console.WriteLine("5 days / 3h forecast for {0}:", forecast.City.CityName);
+
+                        foreach (Forecast fc in forecast.ForecastList)
+                        {
+                            Console.WriteLine(fc.TimeTxt);
+                            Console.WriteLine("Temperature:\t{0} celcius", Math.Round(fc.MainData.Temperature - 272.15));
+                            Console.WriteLine("Wind speed:\t{0} m/s from direction: {1}", Math.Round(fc.Wind.Speed), Math.Round(fc.Wind.Degree));
+
+                            foreach (WeatherDescription wd in fc.Descriptions)
+                            {
+                                Console.WriteLine("Description:\t{0}", wd.GroupWeatherCondition);
+                            }
+                            Console.WriteLine("");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error in getting weather forecast!");
                     }
                 }
             } while (!exit);
-
-        }
-
-        static async Task<CurrentWeather> GetWeatherAsync(string path)
-        {
-            path += "&APPID=52087b841a0e19eae02720bc08ef0b5b";
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                var data = await response.Content.ReadAsStringAsync();
-                localWeather = JsonConvert.DeserializeObject<CurrentWeather>(data);
-            }
-            return localWeather;
-        }
-
-        static async Task<Forecast5days> GetForecastAsync(string path)
-        {
-            path += "&APPID=52087b841a0e19eae02720bc08ef0b5b";
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                var data = await response.Content.ReadAsStringAsync();
-                 forecast = JsonConvert.DeserializeObject<Forecast5days>(data);
-            }
-            return forecast;
         }
     }
 }
--- weather-api/model/Weather.cs
+++ weather-api/model/Weather.cs
@@ -3,21 +3,65 @@ using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Ekoodi.Weather.API
 {
     public class Weather
     {
         static HttpClient client = new HttpClient();
-
-        [JsonProperty("3h")]
-        public double RainVolume3h { get; set; }
+        static CurrentWeather localWeather = new CurrentWeather();
+        static Forecast5days forecast = new Forecast5days();
 
         public Weather()
         {
             string url = "http://api.openweathermap.org/data/2.5/";
             client.BaseAddress = new Uri(url);
             client.DefaultRequestHeaders.Accept.Clear();
+        }
+
+        public CurrentWeather GetWeather( string city)
+        {
+            GetWeatherAsync(client.BaseAddress + "weather?q=" + city).Wait();
+            return localWeather;
+        }
+
+        public Forecast5days GetForecast(string city)
+        {
+            GetForecastAsync(client.BaseAddress + "forecast?q=" + city).Wait();
+            return forecast;
+        }
+
+        static async Task<CurrentWeather> GetWeatherAsync(string path)
+        {
+            path += "&APPID=52087b841a0e19eae02720bc08ef0b5b";
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                localWeather = JsonConvert.DeserializeObject<CurrentWeather>(data);
+            }
+            else
+            {
+                localWeather.Cod = "000";
+            }
+            return localWeather;
+        }
+
+        static async Task<Forecast5days> GetForecastAsync(string path)
+        {
+            path += "&APPID=52087b841a0e19eae02720bc08ef0b5b";
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                forecast = JsonConvert.DeserializeObject<Forecast5days>(data);
+            }
+            else
+            {
+                forecast.Cod = "000";
+            }
+            return forecast;
         }
     }
 }
